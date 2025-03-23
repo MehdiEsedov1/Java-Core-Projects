049db9b833f12531a948dda18142da9b6afce94b
MehdiEsedov1 <mehdiesedof@gmail.com>
2025-03-23 15:13:58 +0400

    Test

--- CarManagement/src/main/java/org/example/AppException.java
+++
@@ -1,7 +0,0 @@
-package main.java.org.example;
-
-public class AppException extends RuntimeException {
-    public AppException(Exceptions e) {
-        super(e.toString());
-    }
-}
--- CarManagement/src/main/java/org/example/GlobalVariables.java
+++
@@ -1,7 +0,0 @@
-package main.java.org.example;
-
-import java.util.ArrayList;
-
-public class GlobalVariables {
-    public static ArrayList<Car> cars = new ArrayList<>();
-}
